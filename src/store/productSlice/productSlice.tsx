d4313668b483ec6b313ea9494dd060fa24a77c67
mryevhenii07 <mrit.works07@gmail.com>
2023-03-23 17:50:21 +0200

    start project

---
+++ src/store/productSlice/productSlice.tsx
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit'
+// import type { PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from '../store'
+
+
+interface CounterState {
+  value: number
+}
+
+const initialState: CounterState = {
+  value: 0,
+}
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+
+  },
+})
+
+export const {  } = counterSlice.actions
+
+export const selectCount = (state: RootState) => state.counter.value
+
+export default counterSlice.reducer
