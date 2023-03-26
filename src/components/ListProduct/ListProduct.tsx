d7c4516f952753c58e850257c330619014d1bcfb
mryevhenii07 <mrit.works07@gmail.com>
2023-03-26 21:28:52 +0300

    added skeleton

--- src/components/ListProduct/ListProduct.tsx
+++ src/components/ListProduct/ListProduct.tsx
@@ -3,21 +3,29 @@ import React, { useEffect, useState } from 'react'
 import { MyProduct } from '../../types/interface';
 import ItemProduct from '../ItemProduct/ItemProduct';
 import s from './ListProduct.module.css';
+import Skeleton from './Skeleton';
 
 type Props = {}
 
 const ListProduct = (props: Props) => {
-
     const [products,setProducts]=useState<MyProduct[]>()
+    const [isLoading,setIsLoading]=useState(true)
     useEffect(()=>{
         fetch('https://dummyjson.com/products')
         .then(res => res.json())
-        .then(json => setProducts(json.products))
+        .then(json => {
+          setProducts(json.products)
+          setIsLoading(false)
+        })
+
+      
     },[])
   
   return (
     <div className={s.wrapper}>
-        {products?.map((product)=> <ItemProduct key={product.id} {...product}/>)}
+        {/* {products?.map((product)=> <Skeleton key={product.id} {...product}/>)} */}
+        {/* {products?.map((product)=> <ItemProduct key={product.id} {...product}/>)} */}
+   {isLoading ? [...new Array(8)].map((_,index) => <Skeleton key={index}/>) : products?.map((product)=> <ItemProduct key={product.id} {...product}/>)}
     </div>
   )
 }
