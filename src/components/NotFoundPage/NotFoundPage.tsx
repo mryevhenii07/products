08a1a877863840281cb2aaf83718e3b20a2560cf
mryevhenii07 <mrit.works07@gmail.com>
2023-03-28 23:42:47 +0300

    added notPageFound

--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
-
-type Props = {}
+import React from 'react';
+import { Link } from 'react-router-dom';
+import s from './NotFoundPage.module.css';
+type Props = {};
 
 const NotFoundPage = (props: Props) => {
   return (
-    <div>NotFoundPage</div>
-  )
-}
+    <section className="page_404">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 ">
+            <div className="col-sm-10 col-sm-offset-1  text-center">
+              <div className="four_zero_four_bg">
+                <h1 className="text-center ">404</h1>
+              </div>
 
-export default NotFoundPage
+              <div className="contant_box_404">
+                <h3 className="h2">Look like you're lost</h3>
+
+                <p>the page you are looking for not avaible!</p>
+
+                <Link to="products" className={s.link}>
+                  <button className={s.button}>Go to Home</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default NotFoundPage;
