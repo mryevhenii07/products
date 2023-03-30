def7be97d1a80695532d64f8f5fd2c9cae33bddf
mryevhenii07 <mrit.works07@gmail.com>
2023-03-30 14:55:33 +0300

    fixed media

--- src/components/ListProduct/Skeleton.tsx
+++ src/components/ListProduct/Skeleton.tsx
@@ -1,25 +1,24 @@
-import React from "react"
-import ContentLoader from "react-content-loader"
+import React from 'react';
+import ContentLoader from 'react-content-loader';
 import s from '../ItemProduct/ItemProduct.module.css';
 
 const Skeleton = () => (
-  <ContentLoader 
-  className={s.wrapper}
+  <ContentLoader
+    className={s.wrapper}
     speed={3}
-    width={335}
+    width={343}
     height={450}
-    viewBox="0 0 335 450"
+    viewBox="0 0 343 450"
     backgroundColor="#f3eded"
-    foregroundColor="#c7c6c6"
-  >
-    <rect x="0" y="209" rx="10" ry="10" width="335" height="25" /> 
-    <rect x="0" y="244" rx="10" ry="10" width="335" height="35" /> 
-    <rect x="0" y="0" rx="0" ry="0" width="335" height="200" /> 
-    <rect x="0" y="290" rx="0" ry="0" width="217" height="30" /> 
-    <rect x="0" y="340" rx="0" ry="0" width="139" height="48" /> 
-    <rect x="280" y="342" rx="0" ry="0" width="35" height="35" /> 
+    foregroundColor="#c7c6c6">
+    <rect x="0" y="209" rx="10" ry="10" width="343" height="25" />
+    <rect x="0" y="244" rx="10" ry="10" width="343" height="35" />
+    <rect x="0" y="0" rx="0" ry="0" width="343" height="200" />
+    <rect x="0" y="290" rx="0" ry="0" width="217" height="30" />
+    <rect x="0" y="340" rx="0" ry="0" width="139" height="48" />
+    <rect x="280" y="342" rx="0" ry="0" width="35" height="35" />
     <rect x="0" y="410" rx="0" ry="0" width="222" height="20" />
   </ContentLoader>
-)
+);
 
-export default Skeleton
+export default Skeleton;
--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -5,27 +5,19 @@ type Props = {};
 
 const NotFoundPage = (props: Props) => {
   return (
-    <section className="page_404">
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12 ">
-            <div className="col-sm-10 col-sm-offset-1  text-center">
-              <div className="four_zero_four_bg">
-                <h1 className="text-center ">404</h1>
-              </div>
+    <section className={s.pageFour}>
+      <div className={s.fourZeroFourBg}>
+        <h1 className="text-center ">404</h1>
+      </div>
 
-              <div className="contant_box_404">
-                <h3 className="h2">Look like you're lost</h3>
+      <div className={s.contantBoxFour}>
+        <h3 className="h2">Look like you're lost</h3>
 
-                <p>the page you are looking for not avaible!</p>
+        <p>the page you are looking for not avaible!</p>
 
-                <Link to="products" className={s.link}>
-                  <button className={s.button}>Go to Home</button>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Link to="products" className={s.link}>
+          <button className={s.button}>Go to Home</button>
+        </Link>
       </div>
     </section>
   );
