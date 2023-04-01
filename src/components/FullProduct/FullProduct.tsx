2b54e5b13847127491f0bcedc1c04a4d53ef7d90
mryevhenii07 <mrit.works07@gmail.com>
2023-04-01 22:45:17 +0300

    added info fullProduct

--- src/components/FullProduct/FullProduct.tsx
+++ src/components/FullProduct/FullProduct.tsx
@@ -1,10 +1,17 @@
 import { useEffect, FC } from 'react';
 import { useParams } from 'react-router-dom';
+import { FaWallet } from 'react-icons/fa';
+import { MdSecurity } from 'react-icons/md';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import StarIcon from '@mui/icons-material/Star';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 import s from './FullProduct.module.css';
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 import { fetchFullProduct } from '../../store/fullProductSlice/fullProductSlice';
+import PrivatBank from '../../assets/images/PrivatBank.jpg';
+import MonoImg from '../../assets/images/mono.png';
 
 const FullProduct: FC = () => {
   const { id } = useParams();
@@ -12,6 +19,7 @@ const FullProduct: FC = () => {
   const { status, fullProduct } = useAppSelector((state) => state.fullProduct);
   const { brand, description, rating, stock, title, price, discountPercentage, images } =
     fullProduct;
+  const value = Number(rating);
   const discount = Math.floor((price / 100) * discountPercentage + price);
   const discountFixed = Number(discount.toFixed()).toLocaleString();
   useEffect(() => {
@@ -33,6 +41,31 @@ const FullProduct: FC = () => {
             </Carousel>
           </div>
           <div className={s.wrapRightBlock}>
+            <div>
+              {' '}
+              <div>
+                <span className={s.brand}> {brand}</span> -<span className={s.title}>{title}</span>:
+                {description}
+              </div>
+              <div className={s.ratingWrap}>
+                <Box
+                  sx={{
+                    width: 100,
+                    display: 'flex',
+                    alignItems: 'center',
+                  }}>
+                  <Rating
+                    name="text-feedback"
+                    value={value}
+                    readOnly
+                    precision={0.5}
+                    emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+                  />
+                </Box>
+
+                <span className={s.reviews}>{stock} відгуків</span>
+              </div>
+            </div>
             <div className={s.wrapRightOne}>
               <div className={s.wrapRightPrice}>
                 <div className={s.discount}>{discountFixed}</div>
@@ -50,7 +83,35 @@ const FullProduct: FC = () => {
               <span className={s.bonusB}>Б</span>{' '}
               <span className={s.bonusBPlus}>+ 49 бонусних $ </span> на рахунок у разі купівлі
             </div>
-            <div></div>
+            <div>
+              <img src={PrivatBank} className={s.bank} alt="img" width={40} height={40} />
+              <img src={MonoImg} className={s.bank} alt="img" width={60} height={40} />
+              <img src={PrivatBank} className={s.bank} alt="img" width={40} height={40} />
+              <img src={PrivatBank} className={s.bank} alt="img" width={40} height={40} />
+              <img src={PrivatBank} className={s.bank} alt="img" width={40} height={40} />
+            </div>
+            <div className={s.wrapRezetka}>
+              <div>
+                Продавець: <span>Rozetka</span>{' '}
+              </div>
+              <div>Rozetka</div>
+            </div>
+            <div className={s.descriptionPayWrap}>
+              <FaWallet className={s.iconWallet} />
+              <p>
+                <span className={s.descriptionTitle}>Оплата.</span> Оплата під час отримання товару,
+                Google Pay, Картою онлайн, Безготівковими для юридичних осіб, Оплатити онлайн
+                соціальною картою "Пакунок малюка", Безготівковий для фізичних осіб, Mastercard,
+                Visa, Оплатити онлайн картою "єПідтримка", Apple Pay
+              </p>
+            </div>
+            <div className={s.descriptionPayWrap}>
+              <MdSecurity className={s.iconSecurity} />
+              <p>
+                <span className={s.descriptionTitle}>Гарантія.</span> 12 місяців Обмін/повернення
+                товару впродовж 14 днів
+              </p>
+            </div>
           </div>
         </div>
       ) : (
